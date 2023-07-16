9f8befee5933f46b6d03600f87db1cdf16510fcb
syed mohammed <85601222+syedmohammedgouse@users.noreply.github.com>
2023-07-17 01:02:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,20 @@ opt.add_experimental_option("prefs", { \
   })
 
 
+opt = Options()
+opt.add_argument("--disable-infobars")
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+opt.add_argument("--start-maximized")
+# Pass the argument 1 to allow and 2 to block
+opt.add_experimental_option("prefs", { \
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 1, 
+    "profile.default_content_setting_values.notifications": 1 
+  })
+
+
 
 
 opt = Options()
